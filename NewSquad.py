b90ddddc4a519c8a5b670b72ad8c3f5261facee0
William Brown <williamjamesb@live.com>
2019-05-30 01:49:46 -0400

    Grenadier Generator

---
+++ NewSquad.py
@@ -0,0 +1,128 @@
+#=============================================================================#
+# PROGRAM: NewSquad.py
+# AUTHOR: Justin Mansell (2018)
+# DESCRIPTION: Generates a list of random guardsmen complete with personalities
+# so you can make your opponent feel bad when he inevitably slaughters them.
+#=============================================================================#
+import names as nm #May have to install: sudo pip install names
+import random as rd
+import sys
+import os
+from RandomGrenadier import *
+
+#Settings
+Ng = int(sys.argv[1]) #Number of guardsmen in the squad (read from console)
+try:
+    squad_type = str(sys.argv[2])
+except:
+    squad_type = 'G'
+rd.seed()
+
+#Put the squad together
+squad = []
+for i in range(0,Ng):
+    gi = Grenadier()
+    gi = basics(gi)
+    gi = demeanor(gi)
+    gi = ambition(gi)
+    gi.skill = []
+    gi.flaw = []
+    gi.friends = []
+    gi.rivals = []
+    gi = traits(gi)
+    squad.append(gi)
+
+#Grenadier squad
+if squad_type == 'G':
+    for g in squad:
+        g.rank = 'Obergrenaider'
+
+#Panzertruppe
+if squad_type == 'P':
+    for g in squad:
+        g.rank = 'Obergrenaider'
+        g = traits(g) #Add extra skills/flaws
+        if len(g.flaw) > 2: #Maximum of 2 flaws
+            g.flaw = g.flaw[0:2]
+
+#Generate friends/rivals
+for gi in squad:
+    Nfriends = rd.randint(0,2)
+    Nrivals = rd.randint(0,2)
+    if gi.personality in ['Quiet','Loner','Backwater','Psychotic']:
+        Nfriends = rd.randint(0,1)
+    if gi.personality in ['Spiteful','Sympathizer','Slacker','Thief',\
+    'Narcissist','Strict','Leech','Never bathes','Incompetent','Braggart']:
+        Nfriends = rd.randint(0,1)
+        Nrivals = Nrivals + rd.randint(0,2)
+    if gi.personality in ['Affable','Mentor','Heroic'] or 'Cook' in gi.skill:
+        Nfriends = Nfriends + rd.randint(0,2)
+
+
+    gi.friends = []
+    gi.rivals = []
+    j = 0
+    while len(gi.friends) < Nfriends and j < 10:
+        f = rd.choice(squad)
+        if f.name in gi.friends or f.name == gi.name:
+            j = j+1
+            continue
+        else:
+            gi.friends.append(f.name)
+    j = 0
+    while len(gi.rivals) < Nrivals and j < 10:
+        f = rd.choice(squad)
+        if f.name in gi.friends or f.name in gi.rivals or f.name == gi.name:
+            j = j+1
+            continue
+        else:
+            gi.rivals.append(f.name)
+
+#Appoint the sergeant
+if squad_type in ['G','P']:
+    serg = 0 #No sergeant yet
+    for i in range(0,Ng):
+        gi = squad[i]
+        if gi.age > 18 and rd.random() < 0.2:
+            # specialists = [
+            # 'Weapon specialist',
+            # 'Weapon specialist',
+            # 'Medic',
+            # 'Combat engineer',
+            # 'Vox operator',
+            # 'Standard bearer'
+            # ]
+            specialists = ['Obergrenaider']
+            gi.rank = rd.choice(specialists)
+        if gi.age < 21 or len(gi.skill) < 1:
+            continue
+        if serg == 0:
+            gi.rank = 'Unteroffizier'
+            serg = 1
+
+#Save the squad
+for i in range(0,1000):
+    fname = 'squad' + str(rd.randint(1,999)) + '.txt'
+    if not os.path.isfile(fname): break
+
+f = open(fname,'w')
+for i in range(0,Ng):
+    gi = squad[i]
+    f.write('Name: '+gi.name+'\n')
+    f.write('Age: '+str(gi.age)+'\n')
+    f.write('Rank: '+gi.rank+'\n')
+    f.write('Battles: '+str(gi.battles)+'\n')
+    f.write('Personality: '+gi.personality+'\n')
+    f.write('Favorite food: '+gi.fav_food+'\n')
+    f.write('Wants to: '+str(gi.ambition)+'\n')
+    f.write('Skills: '+str(gi.skill)+'\n')
+    f.write('Flaws: '+str(gi.flaw)+'\n')
+    f.write('Liked by: '+str(gi.friends)+'\n')
+    f.write('Disliked by: '+str(gi.rivals)+'\n')
+    f.write('Confirmed kills: '+str(gi.confirmed_kills)+'\n')
+    f.write('Awards: '+str(gi.awards)+'\n')
+    f.write('Remarks: '+str(gi.remarks)+'\n')
+    f.write('\n')
+f.close()
+
+#END
---
+++ RandomGrenadier.py
@@ -0,0 +1,254 @@
+import random as rd
+import names as nm
+import datetime
+
+#Internal parameters
+
+
+#Guardsmen object
+class Grenadier:
+    sex = None
+    name = None
+    age = None
+    rank = None
+    squad = None
+    personality = None
+    skill = []
+    flaw = []
+    ambition = None
+    friends = []
+    rivals = []
+    fav_food = None
+    confirmed_kills = 0
+    awards = []
+    battles = 0
+    remarks = []
+
+#Generate basics
+def basics(g):
+    g.sex = 'male'
+    g.name = nm.get_full_name(gender = g.sex)
+    g.age = rd.randint(18,28)
+    g.rank = 'Obergrenaider'
+    g.remarks = ['Deployed '+str(datetime.date.today())]
+    return g
+
+#Generate demeanor
+def demeanor(g):
+    demeanors = [
+    'Affable',
+    'Backwater',
+    'Heroic',
+    'Spiteful',
+    'Boisterous',
+    'Braggart',
+    'Cocky',
+    'Dissenter',
+    'Dreamer',
+    'Gambler',
+    'Green',
+    'Incompetent',
+    'Jaded',
+    'Joker',
+    'Loner',
+    'Loose cannon',
+    'Loyal',
+    'Mentor',
+    'Leech',
+    'Never bathes',
+    'Nihilist',
+    'Numb',
+    'Obsessive',
+    'Old',
+    'Optimist',
+    'Pessimist',
+    'Pious',
+    'Psychotic',
+    'Quiet',
+    'Reckless',
+    'Sarcastic',
+    'Sensible',
+    'Slacker',
+    'Smooth',
+    'Steely',
+    'Strict',
+    'Superstitious',
+    'Talkative',
+    'Thief',
+    'Twitchy',
+    'Narcissist',
+    'Sympathizer',
+    'Old',
+    'Green',
+    'Fanatic'
+    ]
+    g.personality = rd.choice(demeanors)
+
+    if g.personality == 'Old':
+        g.age = rd.randint(29,65)
+        g.personality = rd.choice(demeanors)
+    elif g.personality == 'Green':
+        g.age = rd.randint(16,18)
+        g.rank = 'Grenadier'
+
+    #Favorite food
+    foods = [
+    'Schnitzel',
+    'Bratwurst',
+    'Schweinshaxe',
+    'Hasenpfeffer',
+    'Spaetzle',
+    'Bratkartoffeln',
+    'Leberkaese',
+    'Gulasch',
+    'Schwartzwaelder Kirchtorte',
+    'Saurkraut'
+    ]
+    g.fav_food = rd.choice(foods)
+
+    return g
+
+#Generate ambition
+def ambition(g):
+    ambitions = [
+    'marry a distant paramour',
+    'start a business',
+    'get promoted',
+    'travel the world',
+    'earn the respect of their comrades',
+    'earn the respect of their superiors',
+    'make a difference',
+    'bring honor to their family',
+    'make it home',
+    'start a family',
+    'become rich',
+    'become a movie star',
+    'become a sports star',
+    'acquire knowledge',
+    'meet someone famous',
+    'learn to fly',
+    'redeem themselves',
+    'transfer to a different regiment',
+    'protect a friend',
+    'get laid',
+    'write a book',
+    'own their own house',
+    'set a record',
+    'serve up a cold plate of vengeance',
+    'kill communists',
+    'impress the Hauptmann',
+    'sleep in',
+    'just get five god damn minutes of peace and quiet for once',
+    'eat like a king',
+    'be remembered',
+    'die for the Fuhrer',
+    'get back to the good old days',
+    'desert at the first opportunity'
+    ]
+
+    if rd.random() < 0.1:
+        g.ambition = 'survive'
+    else:
+        g.ambition = rd.choice(ambitions)
+
+    return g
+
+#Generate flaws and boons
+def traits(g):
+    nflaws = rd.randint(0,2)
+    nskills = rd.randint(0,2)
+    if g.personality == 'Incompetent':
+        nflaws = nflaws + 1
+    elif g.personality == 'Heroic':
+        nskills = nskills + 1
+
+    skills = [
+    'Nerves of steel',
+    'Tinkerer',
+    'Lucky',
+    'Cook',
+    'Crack shot',
+    'Combat master',
+    'Street fighter',
+    'Lateral thinker',
+    'Lightning reflexes',
+    'Rapid reload',
+    'Bulging biceps',
+    'Heightened senses',
+    'Tough as nails',
+    'Unshakeable faith',
+    'Foresight',
+    'Ambidextrous',
+    'Duelist',
+    'Tireless',
+    'Bookworm',
+    'Observant',
+    'Handyman',
+    'Quick draw',
+    'Hardy',
+    'Born leader',
+    'Unarmed warrior',
+    'Unremarkable',
+    'Silent move',
+    'Good sense of direction',
+    'Sound judgement',
+    'Survivalist',
+    'Athlete',
+    'Good looking',
+    'Unshakeable pride',
+    'Patient',
+    'Musician',
+    'Awesome scar',
+    'Trustworthy',
+    'Hard target',
+    'Die hard',
+    'Cautious',
+    'Zealous',
+    'Mustache',
+    'Beard',
+    'Bald'
+    ]
+
+    flaws = [
+    'Clumsy',
+    'Unlucky',
+    'Fragile',
+    'Hesitant',
+    'Careless',
+    'Pacifist',
+    'Dumb',
+    'Death-wish',
+    'Coward',
+    'Paranoid',
+    'Addict',
+    'Slow',
+    'Oblivious',
+    'Awkward',
+    'Poor discipline',
+    'Bad back',
+    'Allergies',
+    'Ugly',
+    'Poor health',
+    'Weak',
+    'Overweight',
+    'Unbearable guilt',
+    'Tainted',
+    'Poor aim',
+    'Insomniac',
+    'Old wound',
+    'Bad eyesight',
+    'Basically deaf',
+    'Dyslexic',
+    'Illiterate',
+    'Short temper',
+    'Asthma',
+    'Flat footed'
+    ]
+
+    for s in range(0,nskills):
+        g.skill.append(rd.choice(skills))
+
+    for s in range(0,nflaws):
+        g.flaw.append(rd.choice(flaws))
+
+    return g
